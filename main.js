e058a8a5b713178ba853b7cdc05bdc49a18ecfb2
Allan-1308 <allan.eletrotecnico13@gmail.com>
2025-10-23 20:43:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const key = "d26dd116497131c30bb7694ddf41669c"
+
+
+function pesquisarCidade(){
+    let cidade = document.getElementById("cidade").value
+    console.log(cidade);
+    dadosAPI(cidade)
+
+}
+
+async function dadosAPI(cidade) {
+    let dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&units=metric&lang=pt_br`).then(Response => Response.json())
+    console.log(dados)
+    trocarDados(dados)
+}
+
+// Atualiza os dados na tela
+
+function trocarDados(dados){
+    document.getElementById("nome-cidade").innerHTML = "Tempo em: " + dados.name
+    document.getElementById("img-icon").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png`
+    document.getElementById("umidade").innerHTML = "Umidade relativa do ar: "+ dados.main.humidity + "%"
+}
